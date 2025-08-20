13e1100871d64c0c1ca9af9d20b8d5cb000aa6e6
22300104 <rlatpwnd0049@naver.com>
2025-08-21 04:13:34 +0900

    취약 코드 예제 추가

--- b.py
+++ b.py
@@ -1,16 +1,9 @@
-def celsius_to_fahrenheit(celsius):
-  """섭씨 온도를 화씨로 변환합니다."""
-  fahrenheit = (celsius * 9/5) + 32
-  return fahrenheit
+import sqlite3
 
-# 사용자로부터 섭씨 온도 입력받기
-c_temp = float(input("섭씨 온도를 입력하세요: "))
-f_temp = celsius_to_fahrenheit(c_temp)
-
-print(f"섭씨 {c_temp}°C는 화씨 {f_temp:.2f}°F 입니다.")
-
-password='1234'
-input(password)
-## 이건 test branch 코드입니다.
-## 차이점 추가
-## 차이점 추가 11시 48분
+def get_user(username):
+    db = sqlite3.connect("example.db")
+    cursor = db.cursor()
+    # 사용자 입력을 그대로 쿼리 문자열에 삽입합니다.
+    query = f"SELECT * FROM users WHERE username = '{username}'"
+    cursor.execute(query)
+    return cursor.fetchone()
