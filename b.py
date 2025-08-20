4cfebcb8b23b27f4a846d702fd993ddcebd3c239
22300104 <rlatpwnd0049@naver.com>
2025-08-20 22:35:29 +0900

    b.py 변경

--- b.py
+++ b.py
@@ -1,10 +1,10 @@
-def fahrenheit_to_celsius(fahrenheit):
-  """화씨 온도를 섭씨로 변환합니다."""
-  celsius = (fahrenheit - 32) * 5/9
-  return celsius
+def celsius_to_fahrenheit(celsius):
+  """섭씨 온도를 화씨로 변환합니다."""
+  fahrenheit = (celsius * 9/5) + 32
+  return fahrenheit
 
-# 사용자로부터 화씨 온도 입력받기
-f_temp = float(input("화씨 온도를 입력하세요: "))
-c_temp = fahrenheit_to_celsius(f_temp)
+# 사용자로부터 섭씨 온도 입력받기
+c_temp = float(input("섭씨 온도를 입력하세요: "))
+f_temp = celsius_to_fahrenheit(c_temp)
 
-print(f"화씨 {f_temp}°F는 섭씨 {c_temp:.2f}°C 입니다.")
+print(f"섭씨 {c_temp}°C는 화씨 {f_temp:.2f}°F 입니다.")
